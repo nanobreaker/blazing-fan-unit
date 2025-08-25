2513a588352d6a7129e8d5d5c534a9b8322ca114
nanobreaker <nan0br3aker@gmail.com>
2025-08-25 13:56:16 +0300

    configure emc2101 fan settings

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,17 @@
 use assign_resources::assign_resources;
 use embassy_executor::Spawner;
 use embassy_rp::config::Config;
+use embassy_rp::gpio::{Input, Output, Pull};
 use embassy_rp::i2c::{self, I2c};
 use embassy_rp::peripherals::{self, I2C0, PIO0, UART0, UART1};
 use embassy_rp::pio::Pio;
 use embassy_rp::pio_programs::ws2812::{PioWs2812, PioWs2812Program};
 use embassy_rp::uart::{self, BufferedInterruptHandler, BufferedUart};
 use embassy_rp::{bind_interrupts, Peri};
+use embassy_time::{Duration, Ticker, Timer};
 use emc2101::EMC2101;
+use fugit::Rate;
+use heapless::Vec;
 use smart_leds::RGB8;
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
@@ -63,44 +67,90 @@ assign_resources! {
     },
 }
 
+#[derive(PartialEq)]
+enum Mode {
+    Auto,
+    Manual,
+}
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Config::default());
     let r = split_resources! {p};
 
     // init neopixel
-    let Pio { mut common, sm0, .. } = Pio::new(r.pixel.pio, Irqs);
-    let data = [RGB8::default(); 2];
-    let program = PioWs2812Program::new(&mut common);
-    let mut ws2812 = PioWs2812::<PIO0, 0, 2>::new(&mut common, sm0, r.pixel.dma, r.pixel.led, &program);
-
-    // neopixel example usage for future reference
-    ws2812.write(&data).await;
+    let mut pio = Pio::new(r.pixel.pio, Irqs);
+    let program = PioWs2812Program::new(&mut pio.common);
+    let mut ws2812 = PioWs2812::<PIO0, 0, 2>::new(&mut pio.common, pio.sm0, r.pixel.dma, r.pixel.led, &program);
 
     // init uart0
-    static TX_BUF: StaticCell<[u8; 16]> = StaticCell::new();
-    static RX_BUF: StaticCell<[u8; 16]> = StaticCell::new();
-    let tx_buf = &mut TX_BUF.init([0; 16])[..];
-    let rx_buf = &mut RX_BUF.init([0; 16])[..];
-    let _uart0 = BufferedUart::new(r.uart0.uart, r.uart0.tx, r.uart0.rx, Irqs, tx_buf, rx_buf, uart::Config::default());
+    // static TX_BUF: StaticCell<[u8; 16]> = StaticCell::new();
+    // static RX_BUF: StaticCell<[u8; 16]> = StaticCell::new();
+    // let tx_buf = &mut TX_BUF.init([0; 16])[..];
+    // let rx_buf = &mut RX_BUF.init([0; 16])[..];
+    // let _uart0 = BufferedUart::new(r.uart0.uart, r.uart0.tx, r.uart0.rx, Irqs, tx_buf, rx_buf, uart::Config::default());
+
+    // init fan power
+    let _fan_pwr = Output::new(r.fan.pwr, embassy_rp::gpio::Level::High);
 
     // init emc driver
     let i2c0 = I2c::new_async(r.emc.i2c0, r.emc.scl, r.emc.sda, Irqs, i2c::Config::default());
     let mut emc = EMC2101::new(i2c0).unwrap();
+    emc.enable_tach_input().unwrap();
+    emc.set_fan_pwm(Rate::<u32, _, _>::kHz(25), false).expect("should set fan pwm");
 
     match emc.status() {
         Ok(status) => defmt::info!("emc status {:?}", status),
-        Err(_) => todo!(),
+        Err(_) => defmt::error!("emc not working"),
     }
 
+    // init button
+    let button = Input::new(r.button.btn, Pull::Up);
+
+    let mut data = [RGB8::new(128, 0, 0); 2];
+    let mut ticker = Ticker::every(Duration::from_secs(1));
+    let mut mode = Mode::Auto;
+    let mut power = 0x00;
+
     loop {
-        // implement application logic
-        // read temperature from sensors
-        // read temperature from uart0
-        // read temperature from uart1
-        // determine fan speed based on reading
-        // set fan speed
-        // repeat every second
-        todo!();
+        defmt::info!("tick passed");
+
+        if button.is_low() {
+            mode = match mode {
+                Mode::Auto => Mode::Manual,
+                Mode::Manual => Mode::Auto,
+            }
+        }
+
+        data = match mode {
+            Mode::Auto => [RGB8::new(128, 0, 0); 2],
+            Mode::Manual => [RGB8::new(0, 0, 128); 2],
+        };
+
+        power = match mode {
+            Mode::Auto => 0,
+            Mode::Manual => 99,
+        };
+
+        emc.set_fan_power(power).unwrap();
+
+        ws2812.write(&data).await;
+
+        match emc.fan_rpm() {
+            Ok(rpm) => defmt::info!("fan rpm {=u16}", rpm),
+            Err(e) => defmt::error!("error reading temp internal {:?}", e),
+        }
+
+        match emc.temp_internal() {
+            Ok(t) => defmt::info!("temp internal {=i8}", t),
+            Err(e) => defmt::error!("error reading temp internal {:?}", e),
+        }
+
+        match emc.temp_external_precise() {
+            Ok(t) => defmt::info!("temp external {=f32}C", t),
+            Err(e) => defmt::error!("error reading temp internal {:?}", e),
+        }
+
+        ticker.next().await;
     }
 }
